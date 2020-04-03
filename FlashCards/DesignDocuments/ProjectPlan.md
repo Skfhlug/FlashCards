ec68990d672e8e8d6b0af1214628063f25a7cbfa
[Suparin Fhlug] <[skfhlug@madisoncollege.edu]>
2020-04-02 20:29:36 -0500

    Added something in project plan

--- FlashCards/DesignDocuments/ProjectPlan.md
+++ FlashCards/DesignDocuments/ProjectPlan.md
@@ -1,14 +1,14 @@
 ##Project Plan
 
 Week 1
-- [ ]  Create project repository on Github
-- [ ]  Create Project structure
-- [ ]  Create Data Diagram
-- [ ]  List Technologies that we need to used
+- [x]  Create project repository on Github
+- [x]  Create Project structure
+- [x]  Create Data Diagram
+- [x]  List Technologies that we need to used
 - [ ]  Document application
 Week 2 
 - [ ] Practice working on git branch
-- [ ] Create Database for team project
+- [x] Create Database for team project
 - [ ] Searching about API
 week 3
 - [ ] Link project to an API
---
+++ PreschoolForward/src/main/java/edu/preschool/controller/addRole.java
@@ -0,0 +1,20 @@
+package edu.preschool.controller;
+
+import edu.preschool.entity.User;
+import edu.preschool.persitence.GenericDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet(
+        name = "addRole",
+        urlPatterns = { "/addRole" }
+)
+public class addRole {
+}
