a527fbdcbd9c02347400b6c2031e47b695f9149b
jdone934 <jdone934@hotmail.com>
2020-04-10 23:03:09 -0500

    Adding XML returns. Updating API Class entry

--- src/main/java/edu/team/rest/flashcardsService.java
+++ src/main/java/edu/team/rest/flashcardsService.java
@@ -2,12 +2,11 @@ package edu.team.rest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import edu.team.entity.Flashcard;
 import edu.team.entity.FlashcardSet;
 import edu.team.persistence.GenericDao;
 
-import javax.faces.context.Flash;
-import javax.print.attribute.standard.Media;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,7 +15,7 @@ import java.util.List;
 /**
  * The type Flashcards service.
  */
-@Path("/flashcard")
+@Path("/flashcards")
 public class flashcardsService {
     /**
      * The Flashcard dao.
@@ -27,15 +26,36 @@ public class flashcardsService {
      */
     GenericDao setDao = new GenericDao(FlashcardSet.class);
     /**
-     * The Mapper.
+     * The Json Mapper.
      */
-    ObjectMapper mapper = new ObjectMapper();
+    ObjectMapper jsonMapper = new ObjectMapper();
+
+    /**
+     * The XML Mapper
+     */
+    XmlMapper xmlMapper = new XmlMapper();
 
     private final String SUCCESS = "{'result':'success'}";
     private final String FAILURE = "{'result':'failure'}";
 
     /**
-     * Gets all sets.
+     * Gets all sets, returning xml.
+     *
+     * @return the all sets
+     * @throws JsonProcessingException the json processing exception
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("/sets/xml")
+    public Response getAllSetsXml() throws JsonProcessingException {
+        List<FlashcardSet> sets = setDao.getAll();
+        String output = xmlMapper.writeValueAsString(sets);
+
+        return Response.status(200).entity(output).build();
+    }
+
+    /**
+     * Gets all sets, returning json.
      *
      * @return the all sets
      * @throws JsonProcessingException the json processing exception
@@ -43,9 +63,26 @@ public class flashcardsService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/sets")
-    public Response getAllSets() throws JsonProcessingException {
+    public Response getAllSetsJson() throws JsonProcessingException {
         List<FlashcardSet> sets = setDao.getAll();
-        String output = mapper.writeValueAsString(sets);
+        String output = jsonMapper.writeValueAsString(sets);
+
+        return Response.status(200).entity(output).build();
+    }
+
+    /**
+     * Gets set by id.
+     *
+     * @param id the id
+     * @return the set by id
+     * @throws JsonProcessingException the json processing exception
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("/sets/{id}/xml")
+    public Response getSetByIdXml(@PathParam("id") int id) throws JsonProcessingException {
+        FlashcardSet set = (FlashcardSet) setDao.getById(id);
+        String output = xmlMapper.writeValueAsString(set);
 
         return Response.status(200).entity(output).build();
     }
@@ -60,9 +97,26 @@ public class flashcardsService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/sets/{id}")
-    public Response getSetById(@PathParam("id") int id) throws JsonProcessingException {
+    public Response getSetByIdJson(@PathParam("id") int id) throws JsonProcessingException {
         FlashcardSet set = (FlashcardSet) setDao.getById(id);
-        String output = mapper.writeValueAsString(set);
+        String output = jsonMapper.writeValueAsString(set);
+
+        return Response.status(200).entity(output).build();
+    }
+
+    /**
+     * Search for set response.
+     *
+     * @param searchTerm the search term
+     * @return the response
+     * @throws JsonProcessingException the json processing exception
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("/sets/search/{searchTerm}/xml")
+    public Response searchForSetXml(@PathParam("searchTerm") String searchTerm) throws JsonProcessingException {
+        List<FlashcardSet> sets = setDao.getByPropertyLike("name", searchTerm);
+        String output = xmlMapper.writeValueAsString(sets);
 
         return Response.status(200).entity(output).build();
     }
@@ -77,9 +131,26 @@ public class flashcardsService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/sets/search/{searchTerm}")
-    public Response searchForSet(@PathParam("searchTerm") String searchTerm) throws JsonProcessingException {
+    public Response searchForSetJson(@PathParam("searchTerm") String searchTerm) throws JsonProcessingException {
         List<FlashcardSet> sets = setDao.getByPropertyLike("name", searchTerm);
-        String output = mapper.writeValueAsString(sets);
+        String output = jsonMapper.writeValueAsString(sets);
+
+        return Response.status(200).entity(output).build();
+    }
+
+    /**
+     * Search for set by category response.
+     *
+     * @param searchTerm the search term
+     * @return the response
+     * @throws JsonProcessingException the json processing exception
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("/sets/search/category/{searchTerm}/xml")
+    public Response searchForSetByCategoryXml(@PathParam("searchTerm") String searchTerm) throws JsonProcessingException {
+        List<FlashcardSet> sets = setDao.getByPropertyLike("category", searchTerm);
+        String output = xmlMapper.writeValueAsString(sets);
 
         return Response.status(200).entity(output).build();
     }
@@ -94,9 +165,26 @@ public class flashcardsService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/sets/search/category/{searchTerm}")
-    public Response searchForSetByCategory(@PathParam("searchTerm") String searchTerm) throws JsonProcessingException {
+    public Response searchForSetByCategoryJson(@PathParam("searchTerm") String searchTerm) throws JsonProcessingException {
         List<FlashcardSet> sets = setDao.getByPropertyLike("category", searchTerm);
-        String output = mapper.writeValueAsString(sets);
+        String output = jsonMapper.writeValueAsString(sets);
+
+        return Response.status(200).entity(output).build();
+    }
+
+    /**
+     * Gets cards from set.
+     *
+     * @param id the id
+     * @return the cards from set
+     * @throws JsonProcessingException the json processing exception
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("/sets/cards/{id}/xml")
+    public Response getCardsFromSetXml(@PathParam("id") int id) throws JsonProcessingException {
+        FlashcardSet set = (FlashcardSet) setDao.getById(id);
+        String output = xmlMapper.writeValueAsString(set.getFlashcards());
 
         return Response.status(200).entity(output).build();
     }
@@ -111,9 +199,25 @@ public class flashcardsService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/sets/cards/{id}")
-    public Response getCardsFromSet(@PathParam("id") int id) throws JsonProcessingException {
+    public Response getCardsFromSetJson(@PathParam("id") int id) throws JsonProcessingException {
         FlashcardSet set = (FlashcardSet) setDao.getById(id);
-        String output = mapper.writeValueAsString(set.getFlashcards());
+        String output = jsonMapper.writeValueAsString(set.getFlashcards());
+
+        return Response.status(200).entity(output).build();
+    }
+
+    /**
+     * Gets all cards.
+     *
+     * @return the all cards
+     * @throws JsonProcessingException the json processing exception
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("/cards/xml")
+    public Response getAllCardsXml() throws JsonProcessingException {
+        List<Flashcard> cards = flashcardDao.getAll();
+        String output = xmlMapper.writeValueAsString(cards);
 
         return Response.status(200).entity(output).build();
     }
@@ -127,9 +231,26 @@ public class flashcardsService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/cards")
-    public Response getAllCards() throws JsonProcessingException {
+    public Response getAllCardsJson() throws JsonProcessingException {
         List<Flashcard> cards = flashcardDao.getAll();
-        String output = mapper.writeValueAsString(cards);
+        String output = jsonMapper.writeValueAsString(cards);
+
+        return Response.status(200).entity(output).build();
+    }
+
+    /**
+     * Gets card by id.
+     *
+     * @param id the id
+     * @return the card by id
+     * @throws JsonProcessingException the json processing exception
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("/cards/{id}/xml")
+    public Response getCardByIdXml(@PathParam("id") int id) throws JsonProcessingException {
+        Flashcard card = (Flashcard) flashcardDao.getById(id);
+        String output = xmlMapper.writeValueAsString(card);
 
         return Response.status(200).entity(output).build();
     }
@@ -144,9 +265,9 @@ public class flashcardsService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/cards/{id}")
-    public Response getCardById(@PathParam("id") int id) throws JsonProcessingException {
+    public Response getCardByIdJson(@PathParam("id") int id) throws JsonProcessingException {
         Flashcard card = (Flashcard) flashcardDao.getById(id);
-        String output = mapper.writeValueAsString(card);
+        String output = jsonMapper.writeValueAsString(card);
 
         return Response.status(200).entity(output).build();
     }
